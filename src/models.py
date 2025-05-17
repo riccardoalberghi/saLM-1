4348b8d056487d70d59a4a8b4da4f0db7cb3ad3c
flaviocaroli <flaviocaroli3n9@gmail.com>
2025-05-17 14:40:46 +0200

    corrected models names without dots

--- src/models.py
+++ src/models.py
@@ -16,7 +16,7 @@ MODELS = [
         task="Mathematical reasoning (GSM8K)"
     ),
     ModelInfo(
-        id="Johhny1201/llama3.2_1b_med_QA_2",
+        id="Johhny1201/llama3_2_1b_med_QA_2",
         name="Llama-3.2-1B med QA2",
         path="Johhny1201/llama3.2_1b_med_QA_2",
         parameters=1_000_000_000,
